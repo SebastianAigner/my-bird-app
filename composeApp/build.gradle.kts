1f67feb7a6bf2aef697d63bf5d7782c72560b918
Márton Braun <marton.braun@jetbrains.com>
2023-11-16 16:40:11 +0100

    Set Java version to 11 for Android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "11"
             }
         }
     }
@@ -95,8 +95,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
