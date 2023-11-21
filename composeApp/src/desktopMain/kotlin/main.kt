168886eedeadaf59b5a997dea09d00f50ec6c574
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-11-21 17:19:48 +0100

    Set Desktop window title

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "Bird App") {
         App()
     }
 }
